d2b417b3eabf015751773a54fef149b407f23dba
dtp000 <admin@dtp000.tk>
2023-04-06 09:01:11 +0700

    add cate, home, products view

---
+++ src/LinhKienDienTu.Web.Mvc/Areas/Client/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Abp.AspNetCore.Mvc.Authorization;
+using LinhKienDienTu.Controllers;
+
+namespace LinhKienDienTu.Web.Client.Controllers
+{
+    [Area("Client")]
+    public class CategoriesController : LinhKienDienTuControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
